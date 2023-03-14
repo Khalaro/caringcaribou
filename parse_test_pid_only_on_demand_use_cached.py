cd32aab6df6574205d33bf9e756b45dbf0c34bb9
Khalaro <107428454+Khalaro@users.noreply.github.com>
2023-03-14 15:26:08 -0400

    Update parse_test_pid_only_on_demand_use_cached.py

--- parse_test_pid_only_on_demand_use_cached.py
+++ parse_test_pid_only_on_demand_use_cached.py
@@ -14,6 +14,7 @@ my_dict = {
     'PID': re.compile(r'0x(?P<PID>[0-9A-Fa-f]+) [0-9A-Fa-f]+?'),
     'VALUE': re.compile(r'0x[0-9A-Fa-f]+ (?P<VALUE>[0-9A-Fa-f]+)?'), 
     'PID_KEY': re.compile(r'\bvalue: 7e8[0-9A-Fa-f]{2}([0-9A-Fa-f]+)\b ?'),
+    'PID_KEYv2': re.compile(r'\b0b[0-1]+\b ?'),
     'KEY': re.compile(r'.'),
 }
 
@@ -57,30 +58,24 @@ class client_server_pair:
             return False
     def get_pid_indices(self): # return a list of 32 bools representing support for pids 1-32 on this ECU
         if not os.path.isfile('pid_indices_out.txt'):
-            os.system("obd.query test_pid00 mode=01 pid=00 header=\"'%'\" protocol=6 force=true  >> pid_indices_out.txt"%(self.client_address))
+            os.system("obd.query test_pid00 mode=09 pid="'0'" header="'7df'" bytes=4 formula='bin(bytes_to_int(messages[0].data))' protocol=6 force=true   >> pid_indices_out.txt")
         with open('pid_indices_out.txt') as file:
         pid_indices_file_contents = file.read()
-        pid_index_hex_string=(my_dict['PID_KEY'].findall(pid_indices_file_contents))[0]
-        pid_supported_list = [False,] #there is no pid zero, but we create inxex 0 so later inicies correlate to pids
-        for hex_digit in pid_index_hex_string[0:8]:
-            hex_digit_binary_value=bin(int(hex_digit,16))
-            four_digit_string=(str(hex_digit_binary_value))[2:6]
-            missing_zeros = 4-len(four_digit_string)
-            for i in range(missing_zeros):
-                four_digit_string = '0' + four_digit_string
-            #print(four_digit_string)
-            for bit in four_digit_string:
-                if bit=='0':
-                    pid_supported_list.append(False)
-                else:
-                    pid_supported_list.append(True)
-        #print(pid_supported_list)
-        return pid_supported_list
-        #for index, inx in enumerate(pid_supported_list):
+        pid_index_binary_string=(my_dict['PID_KEY'].findall(pid_indices_file_contents))[0]
+        pid_supported_list = [True,]  #we create index 0 so later indices correlate to pids
+        pid_index_binary_string=((my_dict['PID_KEYv2'].findall(pid_indices_file_contents))[0])[2:]
+        #print(pid_index_hex_string)
+        for binary_digit in pid_index_binary_string:
+            if binary_digit=='1':
+                pid_supported_list.append(True)
+            else:
+                pid_supported_list.append(False)
+        print(pid_supported_list)
+        for index, inx in enumerate(pid_supported_list):
             #print(inx)
-        #    if inx:
-        #        print('supported PID:')
-        #        print(hex(index))
+            if inx:
+                print('supported PID:')
+                print(hex(index))
         
     def query_pids(self, min_pid, max_pid): # ex.  self.query_pid("6182","6184")
         print( 'PID scanning for  ' +  self.client_address +'    '+ self.server_address )
