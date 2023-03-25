9d21445d3fb761c4e90504120c08a37b09452376
Khalaro <107428454+Khalaro@users.noreply.github.com>
2023-03-25 02:46:34 -0400

    Update parse_test_pid_only_on_demand_use_cached.py

--- parse_test_pid_only_on_demand_use_cached.py
+++ parse_test_pid_only_on_demand_use_cached.py
@@ -67,13 +67,15 @@ class client_server_pair:
                 #os.system("autopi obd.query UDS_DID_QUERY header="'%s'" mode="'22'" pid="'%s'" force=True protocol=6 formula='messages[0].data' >> UDS_DID_out_%s_DID_%s.txt"%(self.server_address , standard_did, self.server_address , standard_did))
                 UDS_DID_out_filename="UDS_DID_out_%s_DID_%s.txt"%(self.server_address , standard_did)
                 formula="""formula='messages[0].data[0:]'"""
-                command=("autopi cmd.run 'autopi obd.query UDS_DID_QUERY header=%s mode=22 pid=%s force=True protocol=6 %s >> %s' "%(self.server_address , standard_did, formula, UDS_DID_out_filename))
-                if os.system(command) == 0:
-                    # Check for failure and wait
-                    continue
-                else:
-                    print "os.system ERROR"
-                command=("autopi cmd.run 'autopi obd.query UDS_DID_QUERY header=%s mode=22 pid=%s force=True protocol=6 %s >> %s' "%(self.server_address , standard_did, formula, UDS_DID_out_filename))
+                #command=("autopi cmd.run 'autopi obd.query UDS_DID_QUERY header=%s mode=22 pid=%s force=True protocol=6 %s >> %s' "%(self.server_address , standard_did, formula, UDS_DID_out_filename))
+                #if os.system(command) == 0:
+                #    # Check for failure and wait
+                #    continue
+                #else:
+                #    print "os.system ERROR"
+                print(command)
+                command=('autopi cmd.run "autopi obd.query UDS_DID_QUERY header=%s mode=22 pid=%s force=True protocol=6 %s >> %s" '%(self.server_address , standard_did, formula, UDS_DID_out_filename))
+                os.system(command)
             with open('UDS_DID_out_%s_DID_%s.txt'%(self.server_address ,standard_did)) as file:
                 uds_did_file_contents = file.read()
             if (  (my_dict['GENERIC_VALUE_KEY'].search(uds_did_file_contents)) is not None):
