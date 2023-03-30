ab06cad93d212c6dfee0fe24bc6c1906de124635
Khalaro <107428454+Khalaro@users.noreply.github.com>
2023-03-30 19:20:48 -0400

    Update cantools_use.py

--- cantools_use.py
+++ cantools_use.py
@@ -58,6 +58,7 @@ def main():
     #message = can.Message(arbitration_id=example_message.frame_id, data=data)
     #can_bus.send(message)
     #canddd=444
+    start_time = datetime.datetime.now()
     message = can_bus.recv()
     #db.decode_message(message.arbitration_id, message.data)
     while True:
