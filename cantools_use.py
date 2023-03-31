f97e2ac522b40792368ead78fe59e702ccc2943a
Khalaro <107428454+Khalaro@users.noreply.github.com>
2023-03-31 14:45:31 -0400

    Update cantools_use.py

--- cantools_use.py
+++ cantools_use.py
@@ -66,8 +66,8 @@ def main():
     filters = [{"can_id": 0x215, "can_mask": 0x7FF, "extended": False}, 
 			{"can_id": 0x073, "can_mask": 0x7FF, "extended": False}, 
 			{"can_id": 0x201, "can_mask": 0x7FF, "extended": False}]
-    #can_bus = can.interface.Bus('can0', bustype='socketcan', can_filters=filters)
-    can_bus = can.ThreadSafeBus(channel='can0', bustype='socketcan', can_filters=filters)
+    can_bus = can.interface.Bus('can0', bustype='socketcan', can_filters=filters)
+    #can_bus = can.ThreadSafeBus(channel='can0', bustype='socketcan', can_filters=filters)
     #data = example_message.encode({'Temperature': 250.1, 'AverageRadius': 3.2, 'Enable': 1})
     #message = can.Message(arbitration_id=example_message.frame_id, data=data)
     #can_bus.send(message)
