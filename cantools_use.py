a18c2d153d865c76cdf21129a1bab09bcc7731cb
Khalaro <107428454+Khalaro@users.noreply.github.com>
2023-04-03 10:15:08 -0400

    Update cantools_use.py

--- cantools_use.py
+++ cantools_use.py
@@ -66,32 +66,20 @@ def get_vin_and_protocol( VIN_CODE_LIST , headers, modes, pids, protocols, formu
 def test_read(): 
     can_bus = can.interface.Bus('can0', bustype='socketcan')
     #can_bus = can.ThreadSafeBus(channel='can0', bustype='socketcan', can_filters=filters)
-    #message = Message(data=[1, 2, 3, 4, 5, 6, 7, 8],arbitration_id=533) #533 = 0x215
     message = can_bus.recv()
-    if message.arbitration_id == 0x215:
-        citreon_vin[0:3]= message.data
-    if message.arbitration_id == 0x073:
-        citreon_vin[3:9]= message.data
-    if message.arbitration_id == 0x201:
-        citreon_vin[9:17]= message.data
-    #for indx,val in enumerate(citreon_vin[0:3]):
-    #    val = message.data[indx]
-    #for indx,val in enumerate(citreon_vin[3:9]):
-    #    val = message.data[indx]
-    #for indx,val in enumerate(citreon_vin[9:17]):
-    #    val = message.data[indx]
-    #citreon_vin[0:3]= message.data
-    #citreon_vin[3:9]= message.data
-    #citreon_vin[9:17]= message.data
+    print('OUT 1:')
     print(message)
-    #ascii_bytes=[]
-    #for i,val in enumerate(citreon_vin):
-    #    ascii_bytes.append(bytes(val).decode("ASCII"))
-    #    
-    #print( ascii_bytes )
-    #current_time = datetime.datetime.now()
-    #if (current_time - start_time).total_seconds() >= 10:
-    #    break
+    time.sleep(10.0) 
+    print('OUT 2:')
+    print(message)
+    #if message.arbitration_id == 0x215:
+    #    citreon_vin[0:3]= message.data
+    #if message.arbitration_id == 0x073:
+    #    citreon_vin[3:9]= message.data
+    #if message.arbitration_id == 0x201:
+    #    citreon_vin[9:17]= message.data
+
+
 
 def test_read_v2(): 
     filters = [{"can_id": 0x215, "can_mask": 0x2FF, "extended": False}, 
