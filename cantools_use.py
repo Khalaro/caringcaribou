a5d2281a4e38d83e3fc4240c8ce9cbb6b642ab14
Khalaro <107428454+Khalaro@users.noreply.github.com>
2023-04-04 13:54:24 -0400

    Update cantools_use.py

--- cantools_use.py
+++ cantools_use.py
@@ -22,8 +22,8 @@ class CustomListener(can.Listener):
 	def __init__(self):
 		self.messages = []
 	def on_message_received(self, msg):
-		print("on_message_received:")
-		print(msg)
+		#print("on_message_received:")
+		#print(msg)
 		if msg.arbitration_id in [0x215, 0x073, 0x201]:
 			self.messages.append(msg)
 			print("Received message with arbitration ID {}: {}".format(msg.arbitration_id, msg.data))
@@ -47,11 +47,12 @@ def test_read_v3():
     notifier = can.Notifier(can_bus, [mylistener,])
 
     while True:
-	can_bus.recv()
+	#can_bus.recv()
 	current_time = datetime.datetime.now()
 	if (current_time - start_time).total_seconds() >= 10:
 	    break    
              #can_bus.recv()
+		
     if mylistener.messages is null:
 	print("empty")
     else:
